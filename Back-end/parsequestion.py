7d5cf02560e11b6d641e1acd955c064a14e633b3
Bourdais Rémi <rm.bourdais@gamil.com>
2023-05-24 14:38:15 +0100

    Add intends models

---
+++ Back-end/parsequestion.py
@@ -0,0 +1,199 @@
+from models.model_utils import get_model
+from transformers import AutoTokenizer
+import config 
+import transformers
+
+# Set the transformer verbosity to hide the annoying warnings
+transformers.logging.set_verbosity_error()
+
+# load model and tokenizer
+checkpoint_name = 'bert-base-uncased'
+config.model_name = 'bertdsti'
+config.start_by_loading = True
+config.max_len = 128
+config.load_path = 'trained-models/bert-dsti-ff-new.ptbert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_name, truncation_side='left')
+model, input_function, dataloading_function = get_model(checkpoint_name, tokenizer, None)
+
+
+def add_special_tokens_to_model_and_tokenizer(model, tokenizer, special_tokens, embeddings):
+    # TODO instead of checking for the shared param you should really just have a good way to tell whether the model has some sort of decoder
+    if model is None or hasattr(model, 'shared'):
+        if model is None:
+            for special_token in special_tokens:
+                tokenizer.add_tokens(special_token)
+        return
+    
+add_special_tokens_to_model_and_tokenizer(
+    None,
+    tokenizer,
+    [' Dontcare', '[sys]', '[usr]', '[intent]'],
+    ['I don\'t care', '[SEP]', '[SEP]', '[CLS]']
+)
+
+
+
+def parse_question(questionText):
+    '''
+    Parses the questionText and returns a dictionary with keys for 'must', 'must_not', 'should', and 'filter'.
+    
+    @param questionText: A string with comma-separated category and value pairs.
+    @type questionText: str
+
+    @return: A dictionary with keys for 'must', 'must_not', 'should', and 'filter', each containing a
+             dictionary of parsed category-value pairs. None if the question is not valid.
+    @rtype: dict/None
+    '''
+    o = input_function(tokenizer=tokenizer, question=questionText)
+    tokens = tokenizer.convert_ids_to_tokens(o["input_ids"][0])
+    output = model.get_human_readable_output(o, tokens)
+
+    print(output.get_intent())
+
+    for key in output.value.keys():
+        value = output.get_slot_value_from_key(key)
+        print(key + " = " + value)
+    requests = questionText.split(',')
+
+    parsed_should_question, parsed_must_question, parsed_must_not_question, parsed_filter_question = {}, {}, {}, {}
+    categories_list = ['id', 'family', 'category', 'subcategory', 'gender', 
+                'maincolour', 'secondcolour', 'brand', 'materials', 
+                'shortdescription', 'attributes']
+    
+
+    for request in requests:
+
+        if ':' not in request or request.count(':') > 1:
+            print("Error: invalid request: {}".format(request))
+            return None
+        
+        category_name, category_value = request.split(':')
+        category_name = category_name.strip().lower()
+        category_name = category_name.replace(" ", "")
+        category_value = category_value.strip().lower()
+
+        if not check_category(category_name, categories_list):
+            print("Error: invalid category: {}".format(category_name))
+            return None
+        
+        category_name = change_request(category_name)
+
+
+        if category_name.startswith('mustnot'):
+            parsed_must_not_question[category_name.replace("mustnot","")] = category_value
+        elif category_name.startswith('must'):
+            parsed_must_question[category_name.replace("must","")] = category_value
+        elif category_name.startswith('filter'):
+            parsed_filter_question[category_name.replace("filter","")] = category_value
+        else:
+            parsed_should_question[category_name] = category_value
+
+    return {
+        'must' : parsed_must_question,
+        'must_not' : parsed_must_not_question,
+        'should' : parsed_should_question,
+        'filter' : parsed_filter_question
+    }
+
+# Utility functions
+
+def change_request(request):
+    '''
+    Removes whitespace and underscores from a request string to standardize category names.
+    
+    @param request: A string representing a category name.
+    @type request: str
+
+    @return: A string representing the standardized category name.
+    @rtype: str
+    '''
+    request = request.replace(" ", "")
+    request = request.replace("_", "")
+
+    return request
+
+def unchange_request(request):
+
+    if request == 'subcategory':
+        return 'sub_category'
+    elif request == 'maincolour':
+        return 'main_colour'
+    elif request == 'secondcolour':
+        return 'second_colour'
+    elif request == 'shortdescription':
+        return 'short_description'
+    else:
+        return request
+
+
+def check_category(category_name, categories_list):
+    if category_name.replace("must","") not in categories_list :
+        if category_name.replace("mustnot","") not in categories_list :
+            if category_name.replace("filter","") not in categories_list :
+                return False
+    return True
+
+
+# Query generation
+
+
+def generate_query(question_dict, query_type):
+
+    query = []
+
+    for question in question_dict:
+
+        questionsave = question
+        question = unchange_request(question)
+        field = "product_{}".format(question)
+
+        if query_type == 'must':
+            query.append(generate_must_query(question_dict[questionsave], field))
+        elif query_type == 'should':
+            query.append(generate_should_query(question_dict[questionsave], field))
+        elif query_type == 'must_not':
+            query.append(generate_must_not_query(question_dict[questionsave], field))
+        elif query_type == 'filter':
+            query.append(generate_filter_query(question_dict[questionsave], field))
+            
+    return query
+
+def generate_must_not_query(query, field):
+
+    return{
+        "term": {
+            field: query
+        }
+    }
+
+def generate_filter_query(query, field):
+    
+        return{
+            "term": {
+                field: query
+            }
+        }
+
+def generate_must_query(query, field):
+         
+        return{
+            "multi_match": {
+                "query": query,
+                "fields": field,
+            }
+        }
+
+def generate_should_query(query, field):
+            
+            return{
+                "multi_match": {
+                    "query": query,
+                    "fields": field,
+                }
+            }
+
+''' Test your function
+testQuestion = "gender:Male, maincolour:Red, subcategory:Shirt, brand:Adidas, material:Cotton, size:XL, price:100"
+print(parse_question(testQuestion))
+print(generate_must_query(parse_question(testQuestion)))
+'''
---
+++ Back-end/query.py
@@ -0,0 +1,244 @@
+import numpy as np
+from parsequestion import generate_query
+from PIL import Image
+import requests
+
+import torch.nn.functional as F
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPImageProcessor
+import torch
+# Create the query
+
+def get_query(questionText_dict, has_image,question=None,profile=None):
+    '''
+    Generates a search query based on the given question text dictionary and whether the question includes an image.
+    
+    @param questionText_dict: A dictionary containing information about the user's question text.
+    @type questionText_dict: dict
+    
+    @param has_image: A boolean indicating whether the question includes an image.
+    @type has_image: bool
+    
+    @return: A search query generated based on the user's question text and image.
+    @rtype: dict
+    '''
+    query = {
+        'size': 3,
+        '_source': ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender', 
+                    'product_main_colour', 'product_second_color', 'product_brand', 'product_materials', 
+                    'product_short_description', 'product_attributes', 'product_image_path', 
+                    'product_highlights', 'outfits_ids', 'outfits_products']
+    }
+    if questionText_dict and not has_image:
+        question_must = questionText_dict['must']
+        question_must_not = questionText_dict['must_not']
+        question_should = questionText_dict['should']
+        question_filter = questionText_dict['filter']
+        query['query']= {
+            'bool':{
+                "must": generate_query(question_must, "must"),
+                "must_not": generate_query(question_must_not, "must_not"),
+                "should": generate_query(question_should, "should"),
+                "filter": generate_query(question_filter, "filter")
+            }
+        }
+    elif has_image and not questionText_dict:
+        query['query']=get_similar_images(profile)
+    else:
+        query['query']=get_images_text(question,profile)
+
+    return query
+
+def get_similar_images(profile=None):
+    '''
+    This function retrieves the embeddings for an input image using the CLIP model, and returns a query that can be
+    used to find similar images in an Opensearch index.
+
+    @return: A dictionary with a query with 'knn' key for retrieving similar images based on the input image
+    @rtype: dict
+    '''
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+    qimg = Image.open("images/image.png")
+    input_img= processor(images=qimg, return_tensors="pt")
+    embeddings_img = F.normalize(model.get_image_features(**input_img))
+
+    if profile !=None:
+        embedding_profile_image=0
+        embedding_profile_brand=0
+        embedding_profile_material=0
+        embedding_profile_color=0
+
+        for photo in profile["image"]:
+            pimg = Image.open(requests.get(photo, stream=True).raw)
+            profile_img = processor(images=pimg,return_tensors="pt")
+            embeddings_profileimg = F.normalize(model.get_image_features(**profile_img))
+            embedding_profile_image += embeddings_profileimg[0].detach().numpy()
+
+        for brand in profile["brand"]:
+            pbrand = tokenizer([brand], padding=True, return_tensors="pt")
+            brand_features = F.normalize(model.get_text_features(**pbrand))
+            embedding_profile_brand+=0.05*brand_features[0].detach().numpy()
+        
+        for material in profile["material"]:
+            pmaterial = tokenizer([material], padding=True, return_tensors="pt")
+            material_features = F.normalize(model.get_text_features(**pmaterial))
+            embedding_profile_material+=0.05*material_features[0].detach().numpy()
+
+        for color in profile["main_color"]:
+            pcolor = tokenizer([color], padding=True, return_tensors="pt")
+            color_features = F.normalize(model.get_text_features(**pcolor))
+            embedding_profile_color+=0.5*color_features[0].detach().numpy()
+
+        profile_brand_embeds = embedding_profile_brand.tolist()
+        profile_material_embeds = embedding_profile_material.tolist()
+        profile_main_color_embeds = embedding_profile_color.tolist()
+
+        embeds = torch.tensor(embeddings_img[0].detach().numpy()+ profile_brand_embeds+profile_material_embeds+profile_main_color_embeds)
+        comb_embeds = F.normalize(embeds, dim=0).to(torch.device('cpu')).numpy()
+        return{
+            "knn": {
+                "combined_embedding": {
+                    "vector": comb_embeds,
+                    "k": 3
+                }
+            }
+        }
+
+    else: 
+        return{
+            "knn": {
+                "combined_embedding": {
+                    "vector": embeddings_img[0].detach().numpy(),
+                    "k": 3
+                }
+            }
+        }
+
+def get_images_text(question,profile=None):
+
+    '''
+    This function retrieves the embeddings for an input image  and input text using the CLIP model, and returns a query that can be
+    used to find similar images in an Opensearch index.
+
+    @return: A dictionary with a query with 'knn' key for retrieving similar images based on the input image
+    @rtype: dict
+    '''
+
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+
+    qimg = Image.open("images/image.png")
+    input_img= processor(images=qimg, return_tensors="pt")
+    embeddings_img = F.normalize(model.get_image_features(**input_img))
+
+    inputs = tokenizer([question], padding=True, return_tensors="pt")
+    text_features = F.normalize(model.get_text_features(**inputs))
+    text_embeds = text_features[0].detach().numpy().tolist()
+
+    if profile !=None:
+        embedding_profile_image=0
+        embedding_profile_brand=0
+        embedding_profile_color=0
+        embedding_profile_material=0
+
+        for photo in profile["image"]:
+            pimg = Image.open(requests.get(photo, stream=True).raw)
+            profile_img = processor(images=pimg,return_tensors="pt")
+            embeddings_profileimg = F.normalize(model.get_image_features(**profile_img))
+            embedding_profile_image += embeddings_profileimg[0].detach().numpy()
+
+        for brand in profile["brand"]:
+            pbrand = tokenizer([brand], padding=True, return_tensors="pt")
+            brand_features = F.normalize(model.get_text_features(**pbrand))
+            embedding_profile_brand+=0.05*brand_features[0].detach().numpy()
+        
+        for material in profile["material"]:
+            pmaterial = tokenizer([material], padding=True, return_tensors="pt")
+            material_features = F.normalize(model.get_text_features(**pmaterial))
+            embedding_profile_material+=0.05*material_features[0].detach().numpy()
+
+        for color in profile["main_color"]:
+            pcolor = tokenizer([color], padding=True, return_tensors="pt")
+            color_features = F.normalize(model.get_text_features(**pcolor))
+            embedding_profile_color+=0.05*color_features[0].detach().numpy()
+
+        profile_main_color_embeds = embedding_profile_color.tolist()
+        profile_material_embeds = embedding_profile_material.tolist()
+        profile_brand_embeds = embedding_profile_brand.tolist()
+        embeds = torch.tensor(embeddings_img[0].detach().numpy()+np.array(text_embeds)+ 0.4*embedding_profile_image+profile_brand_embeds+profile_material_embeds+profile_main_color_embeds)
+        comb_embeds = F.normalize(embeds, dim=0).to(torch.device('cpu')).numpy()
+
+        return{
+            "knn": {
+                "combined_embedding": {
+                    "vector": comb_embeds,
+                    "k": 3
+                }
+            }
+        }
+    
+    else:
+        embeds = torch.tensor(embeddings_img[0].detach().numpy()+ np.array(text_embeds))
+        comb_embeds = F.normalize(embeds, dim=0).to(torch.device('cpu')).numpy()
+        return{
+            "knn": {
+                "combined_embedding": {
+                    "vector": comb_embeds,
+                    "k": 3
+                }
+            }
+        }
+
+def profile_query(womenProfile, menProfile, kidsProfile, beautyProfile, categ):
+   query = {'should': [], 'must': []}
+
+   profiles = [womenProfile, menProfile, kidsProfile, beautyProfile]
+   genders = ['WOMEN', 'MEN', 'KIDS', 'BEAUTY']
+
+   for profile, gender in zip(profiles, genders):
+      if profile:
+         query['should'].append({
+               "multi_match": {
+                  "query": gender,
+                  "fields": 'product_gender',
+               }
+         })
+
+   if categ == 'Shoes':
+      query['must'].append({
+            "multi_match": {
+               "query": 'Shoes',
+               "fields": 'product_family',
+            }
+      })
+   elif categ == 'Pants':
+    query['must'].append({
+        "multi_match": {
+            "query": 'Trousers',
+            "fields": 'product_category',
+        }
+   })
+   else:
+      query['must'].append({
+            "multi_match": {
+               "query": categ,
+               "fields": 'product_category',
+            }
+      })
+   return {
+        'size': 1000,
+        '_source': ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender', 
+                    'product_main_colour', 'product_second_color', 'product_brand', 'product_materials', 
+                    'product_short_description', 'product_attributes', 'product_image_path', 
+                    'product_highlights', 'outfits_ids', 'outfits_products'],
+        'query': {
+            'bool': {
+                'must': query['must'],
+                'must_not': [],
+                'should': query['should'],
+                'filter': []
+            }
+        }
+    }
