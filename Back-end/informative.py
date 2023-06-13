8a01dbd54d0a58dac2646a414e6ba3814edc3ad2
Bourdais Rémi <rm.bourdais@gamil.com>
2023-06-13 10:14:52 +0100

    change information

--- Back-end/informative.py
+++ Back-end/informative.py
@@ -1,15 +1,39 @@
 def getInformativeText(previous_products,intend):
-    print(previous_products)
-    text = "In the previous recommendations, you have:"
-    for product in previous_products:
-        text += " " + product['_source']["product_short_description"] + ","
-        text += " from the brand " + product['_source']["product_brand"] + ","
-        text += " in the color " + product['_source']["product_main_colour"] + ","
-        text += " fabric " 
-        for material in product['_source']["product_materials"]:
-            text += material + " ,"
-        text += "as well as"
-        
+    if "user_qa_product" in intend:
+        if intend == "user_qa_product_composition":
+            text = "The previous recommendations are made of "
+        elif intend == "user_qa_product_description" or intend == "user_qa_product_information":
+            text = "The previous recommendations are "
+        else:
+            text = "Sorry, I don't have the measurements of the previous recommendations. please look it up on the website."
+            return text
+    elif "user_qa_check_information" == intend:
+        text = "We are sure that the previous recommendations are "
+    elif "user_inform_product_id" == intend:
+        text = "The id of the previous recommendations are "
+    else:
+        text = "The attributes of the previous recommendations are "
+
+    for i,product in enumerate(previous_products):
+        if intend == "user_qa_product_composition":
+            text += " " + product['_source']["product_materials"][0] + " "
+        elif intend == "user_qa_product_description" or intend == "user_qa_product_information":
+            text += " " + product['_source']["product_short_description"] + " "
+        elif intend == "user_qa_check_information":
+            text += " " + product['_source']["product_short_description"] + "from " + product['_source']["product_brand"] + " "
+        elif intend == "user_inform_product_id":
+            text += " " + str(product['_source']["product_id"]) + " "
+        else:
+            text += " " + str(product['_source']["product_materials"][0]) + "in " + str(product['_source']["product_main_colour"]) + " "
+        if i == 0:
+            text += "for the first one,"
+        elif i == 1:
+            text += "for the second one,"
+        else:
+            text += "for the last one."
+    return text
     
-    return text[:-12] + "."
+        
+
+    
         
